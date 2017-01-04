b7cb403b24c455bebe1efe8e8c1af1880dfc62d2
Nima Shahri <Nima.Rahmani@gmail.om>
2017-01-04 22:43:43 +1300

    some cleaning

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,70 +3,64 @@ var Generator = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var prompts = require('./prompts');
-var path = require("path");
+var path = require('path');
 
 module.exports = Generator.extend({
-  initializing(){
-  },
+    prompting: function () {
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the stellar ' + chalk.red('generator-rollup-init') + ' generator!'
+        ));
 
-  prompting: function () {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the stellar ' + chalk.red('generator-rollup-init') + ' generator!'
-    ));
+        return this.prompt(prompts).then(function (props) {
+            let entry = path.parse(props.entry);
+            props.defaultLicense = 'MIT';
+            props.description = '';
+            props.entryPath = entry.dir;
+            props.entryTest = path.resolve(entry.dir, entry.name + '.spec' + entry.ext);
+            this.props = props;
 
-    return this.prompt(prompts).then(function (props) {
-      let entry = path.parse(props.entry);
+            this.composeWith('license', props);
+        }.bind(this));
+    },
 
-      // To access props later use this.props.someAnswer;
-      props.defaultLicense = 'MIT';
-      props.description = '';
-      props.entryPath = entry.dir;
-      props.entryTest = path.resolve(entry.dir, entry.name + '.spec' + entry.ext)
-      this.props = props;
-      this.composeWith('license', props);
-    }.bind(this));
-  },
+    writing: function () {
+        this.fs.copyTpl(
+            this.templatePath('*'),
+            this.destinationPath(),
+            this.props
+        );
 
-  writing: function () {
-    this.log(this.props);
+        this.fs.copyTpl(
+            this.templatePath('.*'),
+            this.destinationPath(),
+            this.props
+        );
 
-    this.fs.copyTpl(
-      this.templatePath('*'),
-      this.destinationPath(),
-      this.props
-    );
+        this.fs.copyTpl(
+            this.templatePath('.vscode/*'),
+            this.destinationPath('.vscode/'),
+            this.props
+        );
 
-    this.fs.copyTpl(
-      this.templatePath('.*'),
-      this.destinationPath(),
-      this.props
-    );
+        this.fs.copyTpl(
+            this.templatePath('src/index.js'),
+            this.destinationPath(this.props.entry),
+            this.props
+        );
 
-    this.fs.copyTpl(
-      this.templatePath('.vscode/*'),
-      this.destinationPath('.vscode/'),
-      this.props
-    );
+        this.fs.copyTpl(
+            this.templatePath('src/index.spec.js'),
+            this.destinationPath(this.props.entryTest),
+            this.props
+        );
+    },
 
-    this.fs.copyTpl(
-      this.templatePath('src/index.js'),
-      this.destinationPath(this.props.entry),
-      this.props
-    );
-
-    this.fs.copyTpl(
-      this.templatePath('src/index.spec.js'),
-      this.destinationPath(this.props.entryTest),
-      this.props
-    );    
-  },
-
-  install: function () {
-    this.installDependencies({
-      npm: false,
-      bower: false,
-      yarn: true
-    });
-  }
+    install: function () {
+        this.installDependencies({
+            npm: true,
+            bower: false,
+            yarn: false
+        });
+    }
 });
--- generators/app/prompts.js
+++ generators/app/prompts.js
@@ -2,95 +2,88 @@ var path = require("path");
 const gitc = require('git-config').sync();
 
 module.exports = [
-	{
+    {
         name: 'author',
         message: 'What\'s your name:',
-		default: (gitc.user || {}).name
-      },
-      {
+        default: (gitc.user || {}).name
+    },
+    {
         name: 'email',
         message: 'Your email (optional):',
-		default: (gitc.user || {}).email
-      },
-      {
+        default: (gitc.user || {}).email
+    },
+    {
         name: 'website',
         message: 'Your website (optional):'
-      },
-
-	{
-		message: 'What is the project name?',
-		name: 'name',
-		type: 'input',
-		default: process.cwd().split(path.sep).pop()
-	},
-	{
-		message: 'Where is the project entry file?',
-		help: `Your bundle is generated from the entry file – all its dependencies will be included, along with their dependencies, and so on. The entry file's exports become the bundle's exports.`,
-		name: 'entry',
-		type: 'input',
-		default: 'src/index.js'
-	},
-
-	{
-		message: 'Generate a UMD build?',
-		help: `Universal Module Definition allows your bundle to run in browsers and in Node.js, and to work with AMD module loaders`,
-		name: 'umd',
-		type: 'confirm',
-		default: true
-	},
-
-	{
-		when ( results ) {
-			return results.umd === true;
-		},
-		message: `Place to write UMD file`,
-		name: 'umdFile',
-		type: 'input',
-		default (results) {
-			return `dist/${results.name}.umd.js`;
-		}
-	},
-
-	{
-		when(results){
-			return !results.umd;
-		},
-		message: 'Generate a CommonJS build?',
-		help: `CommonJS allows your bundle to run in Node.js`,
-		name: 'cjs',
-		type: 'confirm',
-		default: true
-	},
-
-	{
-		when ( results ) {
-			return results.cjs === true;
-		},
-		message: `Place to write CommonJS file`,
-		name: 'commonJsFile',
-		type: 'input',
-		default (results) {
-			return `dist/${results.name}.cjs.js`;
-		}
-	},
-
-	{
-		message: 'Generate an ES module build?',
-		help: `Creating an ES module build allows your bundle to be easily used by other people using Rollup and other ES module bundlers`,
-		name: 'es',
-		type: 'confirm',
-		default: true
-	},
-
-	{
-		when ( results ) {
-			return results.es === true;
-		},
-		message: `Place to write ES module file`,
-		name: 'esFile',
-		type: 'input',
-		default (results) {
-			return `${path.dirname(results.umdFile) || path.dirname(results.commonJsFile) || 'dist'}/${results.name}.es.js`;
-		}
-	}
+    },
+    {
+        message: 'What is the project name?',
+        name: 'name',
+        type: 'input',
+        default: process.cwd().split(path.sep).pop()
+    },
+    {
+        message: 'Where is the project entry file?',
+        help: `Your bundle is generated from the entry file – all its dependencies will be included, along with their dependencies, and so on. The entry file's exports become the bundle's exports.`,
+        name: 'entry',
+        type: 'input',
+        default: 'src/index.js'
+    },
+    {
+        message: 'Generate a UMD build?',
+        help: `Universal Module Definition allows your bundle to run in browsers and in Node.js, and to work with AMD module loaders`,
+        name: 'umd',
+        type: 'confirm',
+        default: true
+    },
+    {
+        when ( results ) {
+            return results.umd === true;
+        },
+        message: `Place to write UMD file`,
+        name: 'umdFile',
+        type: 'input',
+        default (results) {
+            return `dist/${results.name}.umd.js`;
+        }
+    },
+    {
+        when(results){
+            return !results.umd;
+        },
+        message: 'Generate a CommonJS build?',
+        help: `CommonJS allows your bundle to run in Node.js`,
+        name: 'cjs',
+        type: 'confirm',
+        default: true
+    },
+    {
+        when ( results ) {
+            return results.cjs === true;
+        },
+        message: `Place to write CommonJS file`,
+        name: 'commonJsFile',
+        type: 'input',
+        default (results) {
+            return `dist/${results.name}.cjs.js`;
+        }
+    },
+    {
+        message: 'Generate an ES module build?',
+        help: `Creating an ES module build allows your bundle to be easily used by other people using Rollup and other ES module bundlers`,
+        name: 'es',
+        type: 'confirm',
+        default: true
+    },
+    {
+        when ( results ) {
+            return results.es === true;
+        },
+        message: `Place to write ES module file`,
+        name: 'esFile',
+        type: 'input',
+        default (results) {
+            return `${path.dirname(results.umdFile) || path.dirname(results.commonJsFile) || 'dist'}/${results.name}.es.js`;
+        }
+    }
 ];
